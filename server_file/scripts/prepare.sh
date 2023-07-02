2d5497c3d62d2361d357deebbd56511c1d0e9844
bardchen <bardchen@tencent.com>
2023-07-02 08:36:26 +0800

    优化脚本

--- server_file/scripts/prepare.sh
+++ server_file/scripts/prepare.sh
@@ -1,29 +1,54 @@
-mkdir -p /home/bardchen/software/hugo/scripts;
-cd /home/bardchen/software/hugo;
-git clone git@github.com:YourFantasy/blog.git&&cd blog;
-cd server_file/scripts/&&cp git_pull.sh .env  push_algolia_json.js push_algolia_json_en.js /home/bardchen/software/hugo/scripts/;
-cd ../yum.repos.d&&cp *.repo /etc/yum.repos.d/;
+mkdir -p /home/bardchen/software/hugo/scripts
+cd /home/bardchen/software/hugo
+git clone git@github.com:YourFantasy/blog.git
+if [$? -ne 0];then
+    echo "git clone failed"
+    exit 1
+fi
+cd blog/server_file/scripts/&&cp git_pull.sh .env  push_algolia_json.js push_algolia_json_en.js /home/bardchen/software/hugo/scripts/;
+cd ../yum.repos.d&&cp *.repo /etc/yum.repos.d/
 
 # install softwares
-sudo yum update;
-sudo yum install hugo;
-sudo yum install -y nginx;
-sudo update;
-sudo yum install npm&&npm install atomic-algolia --save;
+sudo yum update
+if [$? -ne 0];then
+    echo "update yum failed"
+    exit 1
+fi
+sudo yum install hugo
+if [$? -ne 0];then
+    echo "install hugo failed"
+    exit 1
+fi
+sudo yum install -y nginx
+if [$? -ne 0];then
+    echo "install nginx failed"
+    exit 1
+fi
+sudo update
+if [$? -ne 0];then
+    echo "update yum failed"
+    exit 1
+fi
+sudo yum install npm&&npm install atomic-algolia --save
+if [$? -ne 0];then
+    echo "install npm and atomic-algolia failed"
+    exit 1
+fi
 
 # start nginx
-sudo netstat -tulpn | grep :80;
+sudo netstat -tulpn | grep :80
 sudo netstat -tulpn | grep :443
-sudo systemctl enable nginx;
-sudo systemctl start nginx;
-sudo systemctl status nginx;
-cd ../nginx &&cp nginx.conf /etc/nginx/nginx.conf;
-sudo nginx -s reload;
-sudo systemctl status nginx;
-sudo systemctl restart nginx;
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
+cd ../nginx &&cp nginx.conf /etc/nginx/nginx.conf
+sudo nginx -s reload
+sudo systemctl status nginx
+sudo systemctl restart nginx
 
 # add crontab task
-echo "* * * * * /home/bardchen/software/hugo/scripts/git_pull.sh" | crontab -;
+echo "* * * * * /home/bardchen/software/hugo/scripts/git_pull.sh" | crontab -
+crontab -l
 
 
 
